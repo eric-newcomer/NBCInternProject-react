5f95a4bf1ffabfee33546d208625004ed3d79198
Eric Newcomer <eric.newcomer@nbcuni.com>
2019-07-11 16:51:54 -0700

    Adding dropwdown menus

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -4,10 +4,18 @@ class Overview extends Component {
     render() {
         return (
             <div className="Overview">
-                <div className="Container">
-                    <h1>Media Tech Program Overview</h1>
-                    <p>This will be a description of the Media Tech Program. lorem ipsum dolor blah blah blah</p>
-                    <button className="btn btn-info">Apply Now!</button>
+                <h1>Media Tech Program Overview</h1>
+                <p>This will be a description of the Media Tech Program. lorem ipsum dolor blah blah blah</p>
+                <button className="btn apply" href="/" >
+                    Apply Now <i class="material-icons">arrow_forward</i>
+                </button>
+                <div className="row option">
+                    <div className="column">
+                        <h2>Media Tech Program</h2>
+                    </div>
+                    <div className="column">
+                        <h2>Associate's Program</h2>
+                    </div>
                 </div>
             </div>
         );
