31561dbda9ff66c0a6024e2d0fb39b96bffa6aa6
Eric Newcomer <eric.newcomer@nbcuni.com>
2019-07-12 08:46:02 -0700

    Fix columns

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class Overview extends Component {
     render() {
         return (
             <div className="Overview">
-                <h1>Media Tech Program Overview</h1>
-                <p>This will be a description of the Media Tech Program. lorem ipsum dolor blah blah blah</p>
-                <button className="btn apply" href="/" >
-                    Apply Now <i class="material-icons">arrow_forward</i>
-                </button>
-                <div className="row option">
-                    <div className="column">
-                        <h2>Media Tech Program</h2>
+                <div className="Overview1">
+                    <h1>Media Tech Program Overview</h1>
+                    <p>This will be a description of the Media Tech Program. lorem ipsum dolor blah blah blah</p>
+                    <button className="btn apply" href="/" >
+                        Apply Now <i class="material-icons">arrow_forward</i>
+                    </button>
                     </div>
-                    <div className="column">
-                        <h2>Associate's Program</h2>
-                    </div>
+                <div className="Overview2">
+                    <Container className="justify-content-md-center">
+                        <Row>
+                            <Col><button className="btn"><h3>Media Tech Program</h3></button></Col>
+                            <Col><button className="btn"><h3>Associate's Program</h3></button></Col>
+                        </Row>
+                    </Container>
                 </div>
             </div>
+            
         );
     }
 }
