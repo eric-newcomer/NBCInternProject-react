c8d1ddbab70d50f1e10b936e71f0659bafa67282
Eric Newcomer <eric.newcomer@nbcuni.com>
2019-07-19 14:13:23 -0700

    Add search function

--- src/components/Faqs.js
+++ src/components/Faqs.js
@@ -5,8 +5,10 @@ class Faqs extends Component {
         return (
             <div className="Faqs" id="FAQs">
                 <h1>FAQs</h1>
-                <h3>Q: Where is it?</h3>
-                <h3>Q: How will I get there?</h3>
+                <h3><strong>Who should apply for Associate positions?</strong></h3>
+                <h3>If you are a recent graduate of a college/university and are legally eligible to work in the U.S., you may apply.</h3>
+                <h3><strong>What is the difference between the Intern and Associate’s program?</strong></h3>
+                <h3>Interns are currently enrolled college students while Associates are recent graduates. The associates program is also rotational and 2 years long while the intern program is only 10 weeks in one location. The associates program is an early career development path that leads to a job at NBCU.</h3>
             </div>
         );
     }
