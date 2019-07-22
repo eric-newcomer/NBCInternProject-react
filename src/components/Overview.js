07e6d7cd543e1743abc10b52ac7a9da7398d3708
Eric Newcomer <eric.newcomer@nbcuni.com>
2019-07-22 16:16:17 -0700

    Add apply link

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -6,7 +6,7 @@ class Overview extends Component {
             <div className="Overview">
                     <h1>Media Tech Program Overview</h1>
                     <p>The NBCUniversal MediaTech Program is an experience like no other. We offer a diverse range of opportunities that provide a unique experience across our iconic portfolio of brands. Through unparalleled access to the best in the business, hands-on training and one-of-a-kind networking events, our interns and associates have the chance to influence change. They shape the way we do things. Here you can contribute as content creators, problem solvers & innovators. Here you can learn the power and possibilities of media and technology. Here you can go far.</p>
-                    <button className="btn apply" href="/" >
+                    <button className="btn apply" href="https://www.nbcunicareers.com/find-a-job" target="_blank" >
                         Apply Now <i className="far fa-arrow-right"></i>
                     </button>
             </div>      
